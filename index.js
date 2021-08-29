6c527abcecb14efe311dfbc95100b330586091df
Stiizzy <75148429+Sskitz@users.noreply.github.com>
2021-08-28 20:50:22 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const noblox = require('noblox.js')
+const schedule = require('node-schedule');
+var prompt = require('prompt-sync')();
+var fs = require("fs")
+/// made by stiizzy cat
+/// this is just simple code, not much...
+
+
+var Cum = prompt('Please enter a decal name ');
+var SEX = prompt('Please enter a time [in miliseconds] ');
+console.log("your decal has been scheduled to upload! :)")
+
+async function startApp () {
+
+
+
+
+
+    const currentUser = await noblox.setCookie('Your cookie here') 
+    await new Promise(resolve => setTimeout(resolve, SEX));
+    console.log("uploaded the decal")
+    console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`)
+    noblox.uploadItem(`${Cum}`, 13, fs.createReadStream("./decal.png")) ///// name your finished decal, decal.png
+}
+startApp()
